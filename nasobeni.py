59d35075668d242df41054c2123ee67649ab3742
erzina <68742072+erzina@users.noreply.github.com>
2024-11-29 23:52:23 +0100

    Delete nasobeni.py

--- nasobeni.py
+++
@@ -1,5 +0,0 @@
-#Napiš funkci mult, která bude mít dva číselné parametry. Funkce oba parametry vynásobí a vrátí výsledek.
-def mult(prvni_parametr, druhy_parametr):
-    return prvni_parametr * druhy_parametr
-tocovyjdezmult = mult(2, 6)
-print(tocovyjdezmult)
