4fa2ee09c6a57b81ecbffbf3c6bcdffafebda682
erzina <68742072+erzina@users.noreply.github.com>
2024-11-29 23:49:24 +0100

    Delete dd.py

--- dd.py
+++
@@ -1,5 +0,0 @@
-names = ['Roman', 'Jana', 'Radek', 'Petra', 'Vlasta']
-
-with open('uzivatele.txt', mode='w', encoding='utf-8') as output_file:
-    for name in names:
-        print(name, file=output_file)
